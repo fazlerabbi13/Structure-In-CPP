5a3f78892a1bed91e0b3651eb2f5808966088343
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-23 14:00:50 +0600

    define a structure name personand maked function prototype it name displayData.

---
+++ passStructToFunc.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+struct Person
+{
+    string name;
+    int age;
+    float salary;
+};
+
+void dispalyData(const Person&);
+
+int main(){
+    Person p {"Rabbi",22,80000};
+
+    // dispalyData(p);
+
+    return 0;
+}
+
+// void displayData(const Person& p){
+//     cout << p.name << "\n";
+//     cout << p.age << "\n";
+//     cout << p.salary << "\n";
+// }
