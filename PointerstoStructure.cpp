4d26e25303c5c23f35cb78d4b6e155ec6d253849
fazlerabbi13 <mohammad0132794@gmail.com>
2025-10-23 19:23:27 +0600

    define a structure and make it pointer to structure.

---
+++ PointerstoStructure.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+struct Distance{
+    int feet;
+    float inch;
+};
+
+
+int main(){
+
+    Distance d;
+    Distance* ptr = &d;
+
+    cin >> (*ptr).feet;
+    cin >> (*ptr).inch;
+    
+    cout << (*ptr).feet << "feet " << (*ptr).inch << "inch";
+
+    return 0;
+}
